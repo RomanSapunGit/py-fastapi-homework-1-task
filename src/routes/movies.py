102f7884699307185f973331883937f4921d7d7c
RomanSapunGit <sapunroma23@gmail.com>
2025-09-28 21:29:02 +0200

    Update solution

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import func
+from fastapi import Request
 
 from src.database import get_db, MovieModel
 from src.schemas.movies import MovieListResponseSchema, MovieDetailResponseSchema
@@ -13,20 +14,28 @@ router = APIRouter()
 
 # Write your code here
 @router.get("/movies/", response_model=MovieListResponseSchema)
-async def get_movies_list(page: int = Query(1, ge=1), per_page: int = Query(10, ge=1, le=20),
-                          db: AsyncSession = Depends(get_db)):
+async def get_movies_list(
+        request: Request,
+        page: int = Query(1, ge=1), per_page: int = Query(10, ge=1, le=20),
+        db: AsyncSession = Depends(get_db)
+):
     total_items = await db.execute(select(func.count()).select_from(MovieModel))
-    total_items_count = total_items.scalar()
+    total_items_count = total_items.scalar() or 0
     total_pages = ceil(total_items_count / per_page)
 
     if page > total_pages:
         raise HTTPException(status_code=404, detail="No movies found.")
 
-    prev_page = f"/theater/movies/?page={page - 1}&per_page={per_page}" if page > 1 else None
-    next_page = f"/theater/movies/?page={page + 1}&per_page={per_page}" if page < total_pages else None
+    base_url = str(request.url.replace(query=""))
+    prev_page = f"{base_url}?page={page - 1}&per_page={per_page}" if page > 1 else None
+    next_page = f"{base_url}?page={page + 1}&per_page={per_page}" if page < total_pages else None
 
     start_item_number = (page - 1) * per_page
-    result_query = await db.execute(select(MovieModel).slice(start_item_number, start_item_number + per_page))
+    result_query = await (db
+                          .execute(select(MovieModel)
+                                   .order_by(MovieModel.id.asc())
+                                   .slice(start_item_number, start_item_number + per_page))
+                          )
     result = result_query.scalars().all()
 
     if not result:
